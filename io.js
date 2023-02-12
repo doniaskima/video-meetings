b045711bfaed1a141b5316ba5e692e913db43166
unknown <doniaskima344@gmail.com>
2023-02-12 17:57:44 +0100

    compplicated

--- io.js
+++ io.js
@@ -2,21 +2,34 @@ const socketIo = require("socket.io");
 const debug = require("debug")("io");
 const RoomModel = require("./models/room");
 const config = require("./config");
+const socketioJwt = require("socketio-jwt");
 
 let io = null;
 
-exports.io = function() {
-    return io;
+exports.io = function () {
+  return io;
 };
 
 // ROOMS map keeps track of users connected to a room
 
 const ROOMS = new Map();
 
-
-exports.initialize = function(server) {
-    io = socketIo(server);
-    io.sockets.on("connection", socketioJwt.authorize({
-
-    }));
-}
+exports.initialize = function (server) {
+  io = socketIo(server);
+  io.sockets
+    .on(
+      "connection",
+      socketioJwt.authorize({
+        secret: config.jwt.secret,
+        callback: 15000,
+      })
+    )
+    .on("authenticated", (socket) => {
+      const user = socket.decoded_token;
+      debug(`A user with '${user.identity}' identity connected`);
+      socket.on("disconnecting", () => {
+        debug(`A user with ${user.identity} identity disconnected`);
+        const socketRooms = Object.keys(socket.rooms);
+      });
+    });
+};
