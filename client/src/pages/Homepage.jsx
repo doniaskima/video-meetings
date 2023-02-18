33c0b76d7eda79f9b3e26a034f4b324e9c6118bd
unknown <doniaskima344@gmail.com>
2023-02-18 14:09:34 +0100

    fixing

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Layout from "../components/Layout";
 
 const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
+    return (
+        <Layout>
+            <div className="relative w-full flex flex-col justify-center items-center">
+                <div>
+                    <h1 className="font-mono text-4xl lg:text-5xl text-center font-semibold leading-tight">
+                        Videoma
+                    </h1>
+                    <p className="text-gray">
+                        Video Meetings App
+                    </p>
+                </div>
+            </div>
+        </Layout>
+    )
 }
 
 export default Homepage
