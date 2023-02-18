89ec651ace09619aa43a8caeffa7b2baedd66020
unknown <doniaskima344@gmail.com>
2023-02-18 13:48:45 +0100

    fix CustomNavLink

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     return (
         <div className="flex flex-col w-full min-h-screen text-base lg:text-lg antialiased text-gray-900 bg-white">
             <Header />
-            <main className="flew-1 flex flex-col justify-center items-center w-full max-w-screen-xl mx-auto px-2">
+            <main className="flex-1 flex flex-col justify-center items-center w-full max-w-screen-xl mx-auto px-2">
                 {children}
             </main>
             <Footer />
--- client/src/components/layout/header/CustomNavLink.jsx
+++ client/src/components/layout/header/CustomNavLink.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CustomNavLink = () => {
-    return (
-        <div>CustomNavLink</div>
-    )
-}
+const CustomNavLink = ({ children, to, ...props }) => (
+    <Link
+        {...props}
+        to={to}
+        className="cursor-pointer no-underline text-black font-medium block w-full sm:w-auto text-center p-2"
+    >
+        {children}
+    </Link>
+);
 
-export default CustomNavLink
+export default CustomNavLink;
