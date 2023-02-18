13f60d0b4a5b052c92804af866ac9170a00191d4
unknown <doniaskima344@gmail.com>
2023-02-18 13:51:51 +0100

    fix the header

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import CustomNavLink from './header/CustomNavLink';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+
+    return (
+        <header className="py-4 px-2 sm:px-6">
+            <nav className="w-full max-w-screen-xl mx-auto flex items-center justify-between">
+                <CustomNavLink to="/">Videoma</CustomNavLink>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
