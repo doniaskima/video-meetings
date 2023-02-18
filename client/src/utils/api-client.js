16f66b88b2d9fec85a09fb189c89bf941e5a9329
unknown <doniaskima344@gmail.com>
2023-02-18 14:58:11 +0100

    api-client

---
+++ client/src/utils/api-client.js
@@ -0,0 +1,30 @@
+import { SERVER_URL } from './server_url'
+
+function client(endpoint, { body, ...customConfig } = {}) {
+  const headers = { 'Content-Type': 'application/json' }
+  const config = {
+    method: body ? 'POST' : 'GET',
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  }
+
+  if (body) {
+    config.body = JSON.stringify(body)
+  }
+
+  return window
+    .fetch(`${SERVER_URL}/api/${endpoint}`, config)
+    .then(async (response) => {
+      const data = await response.json()
+      if (response.ok) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+}
+
+export default client
