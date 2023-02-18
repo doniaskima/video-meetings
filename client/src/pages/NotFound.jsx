99077402e6f244ccf0381976739ea672c6c04a42
unknown <doniaskima344@gmail.com>
2023-02-18 15:40:34 +0100

    NotFound page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+      <div className="flex flex-col flex-1 justify-center items-center">
+          <h2 className="text-3xl text-center">404 | Page Not Found</h2>
+   </div>
   )
 }
 
