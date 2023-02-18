dd037f47b5037d525bdd7ac937b02cc0880668f1
unknown <doniaskima344@gmail.com>
2023-02-18 13:37:14 +0100

    fix the Latout

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
-const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+const Layout = ({ children }) => {
+    return (
+        <div className="flex flex-col w-full min-h-screen text-base lg:text-lg antialiased text-gray-900 bg-white">
+            <Header />
+            <main className="flew-1 flex flex-col justify-center items-center w-full max-w-screen-xl mx-auto px-2">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
 }
 
 export default Layout
