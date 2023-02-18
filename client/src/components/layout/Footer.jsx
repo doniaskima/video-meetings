e38389c4674c79ace6ae8d95cecb9a321c340c19
unknown <doniaskima344@gmail.com>
2023-02-18 14:00:34 +0100

    footer

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className="mt-auto py-4 px-2">
+            <div className="flex justify-center space-x-4 py-2">
+                <Link
+                    className="text-sm uppercase font-medium tracking-wide text-gray-600 focus:outline-none focus:text-gray-400"
+                    to="/how-it-works"
+                >
+                How it Works
+                </Link>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
