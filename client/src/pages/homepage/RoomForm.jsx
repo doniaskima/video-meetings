d54d1e446c981858f99e8947706d3b77c6db82aa
unknown <doniaskima344@gmail.com>
2023-02-19 19:31:25 +0100

    fixing

--- client/src/pages/homepage/RoomForm.jsx
+++ client/src/pages/homepage/RoomForm.jsx
@@ -6,7 +6,10 @@ import useRoom from "../../hooks/useRoom";
 
 const RoomForm = () => {
     const { state, dispatch } = useRoom();
+    const [roomName, setRoomName] = useState(state?.roomName || '');
+    const [identity, setIdentity] = useState('');
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     return (
         <>
             {
@@ -16,8 +19,18 @@ const RoomForm = () => {
                     </div>
                 )
             }
+            <div
+                className={`mt-6 px-2 max-w-xs lg:max-w-sm w-full mx-auto ${loading ? 'opacity-50' : 'opacity-100'
+                    }`}
+            >
+                {error?.message && (
+                    <div className="text-center py-2 text-sm font-semibold uppercase text-red-500 tracking-wide">
+                        {error.message}
+                    </div>
+                )}
+            </div>
             <div>
-                
+
             </div>
         </>
     )
