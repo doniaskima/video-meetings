9dba464aa4a4b6e503d599f7d5b75e62f17ab3fc
unknown <doniaskima344@gmail.com>
2023-02-19 20:03:28 +0100

    fixing

--- client/src/utils/api-client.js
+++ client/src/utils/api-client.js
@@ -1,30 +1,30 @@
-import { SERVER_URL } from './server_url'
+import { server_url } from './server_url'
 
 function client(endpoint, { body, ...customConfig } = {}) {
-  const headers = { 'Content-Type': 'application/json' }
-  const config = {
-    method: body ? 'POST' : 'GET',
-    ...customConfig,
-    headers: {
-      ...headers,
-      ...customConfig.headers,
-    },
-  }
+    const headers = { 'Content-Type': 'application/json' }
+    const config = {
+        method: body ? 'POST' : 'GET',
+        ...customConfig,
+        headers: {
+            ...headers,
+            ...customConfig.headers,
+        },
+    }
 
-  if (body) {
-    config.body = JSON.stringify(body)
-  }
+    if (body) {
+        config.body = JSON.stringify(body)
+    }
 
-  return window
-    .fetch(`${SERVER_URL}/api/${endpoint}`, config)
-    .then(async (response) => {
-      const data = await response.json()
-      if (response.ok) {
-        return data
-      } else {
-        return Promise.reject(data)
-      }
-    })
+    return window
+        .fetch(`${server_url}/api/${endpoint}`, config)
+        .then(async(response) => {
+            const data = await response.json()
+            if (response.ok) {
+                return data
+            } else {
+                return Promise.reject(data)
+            }
+        })
 }
 
 export default client
