7670808c195c23d2e4a7daf03a80d846f9fe893f
unknown <doniaskima344@gmail.com>
2023-03-29 04:02:36 +0100

    feat(Abiut , HowItWork Pages)

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const About = () => {
   return (
-    <div>About</div>
+     <Layout>
+       <h2 className="text-center text-2xl sm:text-3xl">
+          <p className="mt-8 sm:text-xl text-center">
+             the aim of the project is to help to create video meetings with ease
+          </p>
+       </h2>
+     </Layout>
   )
 }
 
--- client/src/pages/HowItWorks.jsx
+++ client/src/pages/HowItWorks.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Layout from "../components/Layout"
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    <Layout>
+      <h2 className="text-center text-2xl sm:text-3xl">
+        How it Works
+      </h2>
+      <p className="mt-8 sm:text-xl text-center">
+        Create the room or enter existing one , send the link to others and enjoy
+      </p>
+    </Layout>
   )
 }
 
