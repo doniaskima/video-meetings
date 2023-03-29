def602da8886b903226b69dd71122187e7c76686
unknown <doniaskima344@gmail.com>
2023-03-29 06:11:08 +0100

    feat(working on MeetingPage)

--- client/src/pages/Meeting.jsx
+++ client/src/pages/Meeting.jsx
@@ -1,11 +1,15 @@
 import React, {useEffect , useRef , useState } from 'react'
-import { useParams , useNavigate } from 'react-router-dom'
+import { useParams  } from 'react-router-dom'
 import Layout from '../components/Layout';
+import RemoteVideo from "../pages/meeting/RemoteVideo";
 
 const Meeting = () => {
   const {roomName} = useParams();
   const socketRef = useRef(null);
-
+  const [isOwner , setIsOwner] = useState(false);
+  const [ringingUser,setRingingUser] = useState(null);
+  const [users, setUsers]=useState({});
+  const [calling , setCalling] =useState(true);
   return (
     <Layout>
       <div>
